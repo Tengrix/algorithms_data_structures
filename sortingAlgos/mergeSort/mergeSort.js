19b2343fb35a098b4b4e95c7040ae2354d7e34cb
tengrix <damir861@gmail.com>
2025-09-28 21:04:31 -0500

    merge function added(practice)

--- sortingAlgos/mergeSort/mergeSort.js
+++ sortingAlgos/mergeSort/mergeSort.js
@@ -19,3 +19,31 @@ const merge = (left,right) => {
 }
 
 console.log(mergeSort([2,1,4,3,6,5,8,7]))
+
+
+const mergeTest = (arr1, arr2) => {
+    let arr = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            arr.push(arr1[i]);
+            i++;
+        } else {
+            arr.push(arr2[j]);
+            j++
+        }
+    }
+    while (i < arr1.length) {
+        arr.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        arr.push(arr2[j]);
+        j++;
+    }
+    return arr;
+}
+
+console.log(mergeTest([1, 3, 5, 8], [2, 4, 6]))
+// arr.concat(arr1.slice(i)).concat(arr2.slice(j)) alternative return
