3a1e1d4e8dc090dc972af353830ab5f1bfa1250a
tengrix <damir861@gmail.com>
2025-11-12 19:00:10 -0600

    DoublyLinkedList shift and updated pop

--- DataStructures/DoublyLinkedList/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList/DoublyLinkedList.js
@@ -27,15 +27,31 @@ class DoublyLinkedList {
     }
     pop(){
         if(this.length < 1) return undefined;
-        let tail = this.tail;
+        let poppedTail = this.tail;
         if(this.length===1){
             this.tail = null;
             this.head = null;
         }else{
-            this.tail = tail.prev;
+            this.tail = poppedTail.prev;
             this.tail.next = null;
+            poppedTail.prev = null
         }
         this.length--;
-        return tail;
+        return poppedTail;
+    }
+    shift(){
+        if(this.length === 0) return undefined;
+        let shiftedNode = this.head;
+        if(this.length === 1){
+            this.tail = null;
+            this.head = null;
+        }else{
+            this.head = shiftedNode.next;
+            this.head.prev = null;
+            shiftedNode.next = null;
+        }
+
+        this.length--;
+        return shiftedNode;
     }
 }
