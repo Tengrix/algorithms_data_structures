990ad6c5302b0acc8c8f5db41ed2578566eac4bf
tengrix <damir861@gmail.com>
2026-02-09 18:12:24 -0600

    BinarySearchTree(BST) - insert

--- DataStructures/BinarySearchTree/BinarySearchTree.js
+++ DataStructures/BinarySearchTree/BinarySearchTree.js
@@ -10,7 +10,35 @@ class Node {
 class BinarySearchTree {
     constructor() {
         this.root = null;
-
+    }
+    insert(value){
+        let newNode = new Node(value);
+        if(!this.root){
+            this.root = new Node(value);
+            return this;
+        }else{
+            let current = this.root;
+            while (true){
+                if (value === current.value) {
+                    return this; // дубликат — ничего не делаем
+                }
+                if(value < current.value){
+                    if(!current.left){
+                        current.left = newNode
+                        return this
+                    }else{
+                        current = current.left
+                    }
+                }else if(value > current.value){
+                    if(!current.right){
+                        current.right = newNode
+                        return this
+                    }else{
+                        current = current.right
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -18,5 +46,6 @@ const binarySearchTree = new BinarySearchTree();
 binarySearchTree.root = new Node(10);
 binarySearchTree.root.right = new Node(15);
 binarySearchTree.root.left = new Node(5);
+binarySearchTree.insert(3)
 
 console.log(binarySearchTree);
